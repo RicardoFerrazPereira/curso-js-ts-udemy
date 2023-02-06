bbd24bc1e9219f89511fd758b2eaa76e680dc34c
RicardoFerrazPereira <sourvivens@gmail.com>
2023-02-05 21:57:31 -0300

    Exercício FOR

---
+++ secao3-Logica/aula53_Exercicio_For/assets/js/main.js
@@ -0,0 +1,20 @@
+const elementos = [
+    {tag: 'p', texto: 'Gigante da Colina'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 4'},
+    {tag: 'footer', texto: 'Frase 3'},
+];
+
+const container = document.querySelector('.container'); // Selecionar o elementos
+const div = document.createElement('div');  // Criar um elemento ('div'), que vai receber as tags
+
+// 'For' para criar as tags
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i]; // Selecionar
+    let tagCriada = document.createElement(tag); // Criar as tags
+    // tagCriada.innerHTML = texto; // Adicionar o texto na tag
+    tagCriada.innerText = texto; // Adicionar o texto na tag
+    div.appendChild(tagCriada); // Adiciona as "tags" dentro da "div"
+}
+
+container.appendChild(div); // Adiciona a "div" dentro da "container"
