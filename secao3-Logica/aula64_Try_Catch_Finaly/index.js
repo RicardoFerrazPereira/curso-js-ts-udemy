2ab2e981aabc5213c0a753012b1347fc966a4481
RicardoFerrazPereira <sourvivens@gmail.com>
2023-02-15 22:04:45 -0300

    Tratando e lançando erros (try, catch, finally)

--- secao3-Logica/aula64_Try_Catch_Finaly/index.js
+++ secao3-Logica/aula64_Try_Catch_Finaly/index.js
@@ -31,3 +31,42 @@ try {
 }finally {
     console.log('Finally: Eu sempre sou executado');
 }
+
+console.log('########################');
+
+// Cria uma função para retornar a hora
+
+const retornaHora = (data) => {
+  if(!(data instanceof Date)) { // se essa "data" não for uma instâcia de "Date", vai dar "false" , erro
+   //console.log('Não é');       // Para ser instância de "Date", tem que ter o "new"
+    throw new TypeError('Esperando instância de Date');
+  }
+}
+retornaHora(new Date());
+
+const retornaHoraAtual = (data) => {
+  if(data && !(data instanceof Date)) {
+    throw new TypeError('Esperando instância de Date');
+  }
+
+  if (!data) {
+    data = new Date();
+  }
+
+  return data.toLocaleTimeString('en', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  });
+}
+try {
+  const data = new Date('01-01-1970 12:58:12');
+  const hora = retornaHoraAtual();
+  console.log(hora);
+  } catch(e) {
+    // Tratar erro
+    //console.log(e);
+  } finally {
+    console.log('Tenha um bom dia');
+  }
