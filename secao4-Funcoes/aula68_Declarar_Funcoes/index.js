0a3ff46cab4c25ffaeb2d133b95739ad9a0ddb3b
ricardo_ferraz <ricardo.ferraz@medportal.com.br>
2023-02-17 16:22:16 -0300

    Declarando funções

---
+++ secao4-Funcoes/aula68_Declarar_Funcoes/index.js
@@ -0,0 +1,8 @@
+// Declaração de função 
+
+function falaOi() {
+    console.log('Olá Mundo!')
+}
+// (Function hoisting) -> significa que tando posso declarar
+// a função e depois chama-la ou chama-la e depois declarar
+// 
