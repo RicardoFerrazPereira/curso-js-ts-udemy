90ef4e26718695bc01da90c2f717cd938c802aa3
RicardoFerrazPereira <sourvivens@gmail.com>
2023-03-06 22:22:32 -0300

    Funções Construtoras

--- secao4-Funcoes/aula77_Funcoes_Construtoras/index.js
+++ secao4-Funcoes/aula77_Funcoes_Construtoras/index.js
@@ -5,12 +5,18 @@
 // O corpo da função já vai ser objeto que vai ser criado
 // "new" -> cria um objeto vazio, faz o "this" apontar para esse objeto vazio e retorna o objeto para a "variável"
 // Método -> é uma função dentro de um objeto.
+// Atributos privados -> podemos ter coisas que não queremos que estejam disponíveis nos objetos,
+// que só iremos usar dentro da função. Criamos uma variável (const ou let) dentro da função. exemplo: ID
 
 function Pessoa(nome, sobrenome) {
-    // Atributos
+    // Atributos e métodos privados
+    const ID = 123456;
+    const metodoInterno = () => {
+    };  
+    // Atributos públicos
     this.nome = nome;
     this.sobrenome = sobrenome;
-    // Métodos
+    // Métodos públicos
     this.metodo = () => {
         console.log(this.nome + ': sou um método');
     }
