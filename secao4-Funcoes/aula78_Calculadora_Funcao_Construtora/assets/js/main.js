09286d6df42c089b3c922a135f4d1c58905fa097
RicardoFerrazPereira <sourvivens@gmail.com>
2023-03-08 20:49:34 -0300

    Calculadora com Funções Construtoras

--- secao4-Funcoes/aula78_Calculadora_Funcao_Construtora/assets/js/main.js
+++ secao4-Funcoes/aula78_Calculadora_Funcao_Construtora/assets/js/main.js
@@ -1,5 +1,17 @@
 function Calculadora() {
-   this.display = document.querySelector('.display');
+    this.display = document.querySelector('.display');
+
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    }
+
+    this.capturaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if(e.keyCode !== 13) return;
+            this.realizaConta();
+        })
+    }
 
     this.capturaCliques = () => {
         document.addEventListener('click', (event) => {
@@ -7,14 +19,34 @@ function Calculadora() {
 
             if(elemento.classList.contains('btn-num')) this.addNumDisplay(elemento);
             if(elemento.classList.contains('btn-clear')) this.clear();
-            if(elemento.classList.contains('btn-del')) this.del(elemento);
-            if(elemento.classList.contains('btn-eq')) this.realizaConta(elemento);            
+            if(elemento.classList.contains('btn-del')) this.del();
+            if(elemento.classList.contains('btn-eq')) this.realizaConta();            
         });
     };
 
-    this.addNumDisplay = (elemento) => this.display.value += elemento.innerText;   
-    this.clear = () => this.display.value = '';
-    this.inicia = () => this.capturaCliques();   
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if(!conta) {
+                alert('Conta inválida');
+                return;
+            }
+            this.display.value = conta;
+
+        } catch(error) {
+            alert('Conta inválida');
+            return;
+        } 
+    };
+
+    this.addNumDisplay = (elemento) => {
+        this.display.value += elemento.innerText; 
+        this.display.focus();
+
+    }   
+    this.clear = () => this.display.value = '';   
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
 }
 
 const calculadora = new Calculadora();
